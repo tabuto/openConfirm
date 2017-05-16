f97922e0fa61914ab3ca119c233091927936b0b8
tabuto <tabuto83@gmail.com>
2017-05-16 18:37:36 +0200

    Add db test connection

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
 #!flask/bin/python
+
+'''
+DATABASE connection
+       Username: openconfirm
+       Password: POSTGRES_PASSWORD   open-confirm-conf.db.pswd
+  Database Name: openconfirm
+ Connection URL: postgresql://postgresql:5432/
+'''
+
 from flask import Flask
+from flask.ext.restful import Api, Resource
+import pgdb
+import os
+
+hostname = 'postgresql'
+username = 'openconfirm'
+password = os.environ['POSTGRES_PASSWORD']
+database = 'openconfirm'
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+	return "Hello, World!"
+
+
+def testDBConnection():
+	myConnection = pgdb.connect( host=hostname, user=username, password=password, dbname=database )
+	myConnection.close()
+	print 'DB Connection ok'
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',8080)
+	app.run('0.0.0.0',8080)
